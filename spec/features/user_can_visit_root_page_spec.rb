d4ec27d12fa2eb1744c9a1aeaf6b9f24bd319002
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-08-29 09:22:52 -0600

    starting test

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -8,4 +8,9 @@ feature "User can visit root page" do
       expect(page).to have_selector("input[value='Search by zip...']")
     end
   end
+  it 'can generate a list of the 10 closest stations to the entered zip codes within 6 miles sorted by distance, limited to electric and propane, and it should show the name, address, fuel types, distance and access times for each station' do
+    visit '/'
+
+    
+  end
 end
