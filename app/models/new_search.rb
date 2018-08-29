3b64ce55fabce07ecd917968aefec9e00c7da544
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-08-29 10:25:10 -0600

    querying in progress

--- app/models/new_search.rb
+++ app/models/new_search.rb
@@ -7,9 +7,12 @@ class NewSearch
 
   def stations
     conn = Faraday.new(url:'https://developer.nrel.gov')
-    response = conn.get("/api/alt-fuel-stations/v1.json?zip=#{@zip}&limit=10&radius=6&fuel_type=ELEC,LPG&api_key=#{ENV['API-KEY']}")
-    stations = response.body['fuel_stations']
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip}&radius=6&limit=10&fuel_type=ELEC,LPG&api_key=#{ENV['API-KEY']}")
+    stations = JSON.parse(response.body)['fuel_stations']
     binding.pry
+    stations.each do |station|
+      Station.new(station['station_name'], station['street_address'], station['fuel_type_code'], station['access_days_time'], station[''])
+    end
   end
 
 end
